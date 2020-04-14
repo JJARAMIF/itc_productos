d24de2dcfe876d014f6fcd99577c3e08872e87e6
jamil jaramillo <innova_ec@hotmail.com>
2020-04-14 00:24:57 -0500

    update

--- MakeBase64CSV+.sh
+++ MakeBase64CSV+.sh
@@ -3,12 +3,12 @@
 # product_imagen+.csv columns are : externalID, nombre, producto, url
 # product_imagen+.csv separator MUST BE ;
 
-echo \"External ID\",\"Name\",\"product_tmpl_id/id\",\"video_url\",\"Image\" > $2
+echo \"External ID\",\"Name\",\"Product_tmpl_id/id\",\"Video_URL\",\"Image\" > $2
 
 while IFS=";" read f1 f2 f3 f4 ; do
 
-# recopy external ID
-echo -n  $f1,\"$f2\",\"$f3\",\"$f4\", >> $2
+# recopy external ID y Encabezados
+echo -n  $f1,\"$f2\",$f3,$f4, >> $2
 
 #If second  column represents the key to match with the filename, please use this command
 cat $(echo ${f1} | tr -d '\r' | tr -d '"').jpg | base64 --wrap=0 >> $2
