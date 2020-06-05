c947bbd43a69a207f7e8ba17a29c693c9794d3f7
jamil jaramillo <innova_ec@hotmail.com>
2020-06-05 00:26:52 -0500

    Update MakeBase64+.sh

--- MakeBase64+.sh
+++ MakeBase64+.sh
@@ -8,7 +8,7 @@ echo \"External ID\",\"product_tmpl_id/id\",\"name\",\"Image\" > $2
 while IFS=";" read f1 f2 f3 ; do
 
 # recopy external ID y Encabezados
-echo -n  \"$f1-01\", \"$f1\", \"$f2-01\", >> $2
+echo -n  \"$f1_01\", \"$f1\", \"$f2-01\", >> $2
 
 cat $(echo ${f2} | tr -d '\r' | tr -d '"').jpg | base64 --wrap=0 >> $2
 
