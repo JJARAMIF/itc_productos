c3682de677f6d1dbccebf894e2431a37922c46ea
jamil jaramillo <innova_ec@hotmail.com>
2020-06-05 18:54:48 -0500

    update

--- MakeBase64.sh
+++ MakeBase64.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Usage : MakeBase64.sh product_imagen.csv product_base64.csv
-# product_imagen.csv columns are : externalID, nombre, imagen
+# product_imagen.csv columns are : externalID, nombre, jpg
 # product_imagen.csv separator MUST BE ;
 
 ext=".jpg"
