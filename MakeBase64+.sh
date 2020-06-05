593a58f8941c1972dd3c3a51e002c0eaef34f560
jamil jaramillo <innova_ec@hotmail.com>
2020-06-05 02:18:57 -0500

    Update MakeBase64+.sh

--- MakeBase64+.sh
+++ MakeBase64+.sh
@@ -3,6 +3,7 @@
 # product_imagen.csv columns are : externalID, jpg, nombre
 # product_imagen.csv separator MUST BE ;
 
+ext=".jpg"
 a="_product_template"
 b1="_01"
 c1="-01"
@@ -15,20 +16,23 @@ echo \"External ID\",\"product_tmpl_id/id\",\"name\",\"jpg\",\"Image\" > $2
 
 while IFS=";" read f1 f2 f3 ; do
 
-# recopy external ID y Encabezados
-echo -n  $f1$b1,$f1$a,$f2$c1,${f2}$c1 >> $2
+    if [ -f $f2$c1$ext ]; then
+       	# recopy external ID y Encabezados
+	echo -n  $f1$b1,$f1$a,$f2$c1,${f2}$c1 >> $2
 
-cat $(echo ${f2}$c1 | tr -d '\r' | tr -d '"').jpg | base64 --wrap=0 >> $2
+	cat $(echo ${f2}$c1 | tr -d '\r' | tr -d '"').jpg | base64 --wrap=0 >> $2
 
-#Carrier return at end of line
-echo  >> $2
-
-echo -n  $f1$b2,$f1$a,$f2$c2,${f2}$c2 >> $2
-cat $(echo ${f2}$c2 | tr -d '\r' | tr -d '"').jpg | base64 --wrap=0 >> $2
-echo  >> $2
-
-echo -n  $f1$b3,$f1$a,$f2$c3,${f2}$c3 >> $2
-cat $(echo ${f2}$c3 | tr -d '\r' | tr -d '"').jpg | base64 --wrap=0 >> $2
-echo  >> $2
-
+	#Carrier return at end of line
+	echo  >> $2
+    fi
+    if [ -f $f2$c2$ext ]; then
+	echo -n  $f1$b2,$f1$a,$f2$c2,${f2}$c2 >> $2
+	cat $(echo ${f2}$c2 | tr -d '\r' | tr -d '"').jpg | base64 --wrap=0 >> $2
+	echo  >> $2
+    fi
+    if [ -f $f2$c3$ext ]; then
+	echo -n  $f1$b3,$f1$a,$f2$c3,${f2}$c3 >> $2
+	cat $(echo ${f2}$c3 | tr -d '\r' | tr -d '"').jpg | base64 --wrap=0 >> $2
+	echo  >> $2
+    fi
 done < $1
