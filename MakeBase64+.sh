10c2cc68164ce6b4478fff02a6f115563df01d92
jamil jaramillo <innova_ec@hotmail.com>
2020-06-05 01:12:14 -0500

    Update MakeBase64+.sh

--- MakeBase64+.sh
+++ MakeBase64+.sh
@@ -14,7 +14,7 @@ while IFS=";" read f1 f2 f3 ; do
 # recopy external ID y Encabezados
 echo -n  \"$f1$b\", \"$f1$a\", \"$f2$c\", >> $2
 
-cat $(echo ${f2}$c | tr -d '\r' | tr -d '"').jpg | base64 --wrap=0 >> $2
+cat $(echo ${f2} | tr -d '\r' | tr -d '"')$c.jpg | base64 --wrap=0 >> $2
 
 #Carrier return at end of line
 echo  >> $2
