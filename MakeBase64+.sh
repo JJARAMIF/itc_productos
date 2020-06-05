fea52ce13abbdf7db38ef612d64af4f7b376a0e2
jamil jaramillo <innova_ec@hotmail.com>
2020-06-05 01:44:19 -0500

    Update MakeBase64+.sh

--- MakeBase64+.sh
+++ MakeBase64+.sh
@@ -4,17 +4,26 @@
 # product_imagen.csv separator MUST BE ;
 
 a="_product_template"
-b="_01"
-c="-01"
+b1="_01"
+c1="-01"
+b2="_02"
+c2="-02"
+b2="_03"
+c2="-03"
 
 echo \"External ID\",\"product_tmpl_id/id\",\"name\",\"Image\" > $2
 
 while IFS=";" read f1 f2 f3 ; do
 
 # recopy external ID y Encabezados
-echo -n  $f1$b,$f1$a,$f2$c, >> $2
+echo -n  $f1$b1,$f1$a,$f2$c1, >> $2
+cat $(echo ${f2}${c1} | tr -d '\r' | tr -d '"').jpg | base64 --wrap=0 >> $2
 
-cat $(echo ${f2}${c} | tr -d '\r' | tr -d '"').jpg | base64 --wrap=0 >> $2
+echo -n  $f1$b2,$f1$a,$f2$c2, >> $2
+cat $(echo ${f2}${c2} | tr -d '\r' | tr -d '"').jpg | base64 --wrap=0 >> $2
+
+echo -n  $f1$b3,$f1$a,$f2$c3, >> $2
+cat $(echo ${f2}${c3} | tr -d '\r' | tr -d '"').jpg | base64 --wrap=0 >> $2
 
 #Carrier return at end of line
 echo  >> $2
