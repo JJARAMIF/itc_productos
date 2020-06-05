870208970f02980f30b10bdb2d87a88984bb2099
jamil jaramillo <innova_ec@hotmail.com>
2020-06-05 01:10:32 -0500

    Update MakeBase64+.sh

--- MakeBase64+.sh
+++ MakeBase64+.sh
@@ -3,12 +3,16 @@
 # product_imagen.csv columns are : externalID, jpg, nombre
 # product_imagen.csv separator MUST BE ;
 
+a="_product_template"
+b="_01"
+c="-01"
+
 echo \"External ID\",\"product_tmpl_id/id\",\"name\",\"Image\" > $2
 
 while IFS=";" read f1 f2 f3 ; do
 
 # recopy external ID y Encabezados
-echo -n  \"$f1-01\", \"$f1\", \"$f2-01\", >> $2
+echo -n  \"$f1 $b\", \"$f1 $a\", \"$f2 $c\", >> $2
 
 cat $(echo ${f2} | tr -d '\r' | tr -d '"').jpg | base64 --wrap=0 >> $2
 
