4aadc5e70bcc83c41eac71a5d79b0b777d2bc21c
jamil jaramillo <innova_ec@hotmail.com>
2020-06-05 02:04:33 -0500

    Update MakeBase64+.sh

--- MakeBase64+.sh
+++ MakeBase64+.sh
@@ -11,23 +11,23 @@ c2="-02"
 b3="_03"
 c3="-03"
 
-echo \"External ID\",\"product_tmpl_id/id\",\"name\",\"Image\" > $2
+echo \"External ID\",\"product_tmpl_id/id\",\"name\",\"jpg\",\"Image\" > $2
 
 while IFS=";" read f1 f2 f3 ; do
 
 # recopy external ID y Encabezados
-echo -n  $f1$b1,$f1$a,$f2$c1, >> $2
+echo -n  $f1$b1,$f1$a,$f2$c1,${f2}$c1 >> $2
 
 cat $(echo ${f2}$c1 | tr -d '\r' | tr -d '"').jpg | base64 --wrap=0 >> $2
 
 #Carrier return at end of line
 echo  >> $2
 
-echo -n  $f1$b2,$f1$a,$f2$c2, >> $2
+echo -n  $f1$b2,$f1$a,$f2$c2,${f2}${c2} >> $2
 cat $(echo ${f2}${c2} | tr -d '\r' | tr -d '"').jpg | base64 --wrap=0 >> $2
 echo  >> $2
 
-echo -n  $f1$b3,$f1$a,$f2$c3, >> $2
+echo -n  $f1$b3,$f1$a,$f2$c3,${f2}${c3} >> $2
 cat $(echo ${f2}${c3} | tr -d '\r' | tr -d '"').jpg | base64 --wrap=0 >> $2
 echo  >> $2
 
