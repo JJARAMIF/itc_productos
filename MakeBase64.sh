f2ef58dd34ec86538442c693521cb4692773defd
jamil jaramillo <innova_ec@hotmail.com>
2020-06-05 19:10:24 -0500

    update

--- MakeBase64.sh
+++ MakeBase64.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Usage : MakeBase64.sh product_imagen.csv product_base64.csv
-# product_imagen.csv columns are : externalID, nombre, jpg
+# product_imagen.csv columns are : externalID, jpg, nombre
 # product_imagen.csv separator MUST BE ;
 
 ext=".jpg"
@@ -9,13 +9,13 @@ echo \"External ID\",\"Image\" > $2
 
 while IFS=";" read f1 f2 f3 ; do
 
-    if [ -f $f3$ext ]; then
+    if [ -f $f2$ext ]; then
 
 	# recopy external ID
 	echo -n $f1, >> $2
 
 	#If second  column represents the key to match with the filename, please use this command
-	cat $(echo ${f3} | tr -d '\r' | tr -d '"').jpg | base64 --wrap=0 >> $2
+	cat $(echo ${f2} | tr -d '\r' | tr -d '"').jpg | base64 --wrap=0 >> $2
 
 	#Carrier return at end of line
 	echo  >> $2
