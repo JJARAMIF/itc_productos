376b170cb3b07cb5e15c81e499cf9548c6d67511
jjaramif <jamil.jaramillo@hotmail.com>
2020-06-06 01:20:33 +0000

    Actualizacion

--- MakeBase64+.sh
+++ MakeBase64+.sh
@@ -4,7 +4,6 @@
 # product_imagen.csv separator MUST BE ;
 
 
-NUM=1
 ext=".jpg"
 a="_product_template"
 b="_0"
@@ -15,18 +14,21 @@ echo \"External ID\",\"product_tmpl_id/id\",\"name\",\"Image\" > $2
 
 while IFS=";" read f1 f2 f3 ; do
 
-	while [ $NUM -le 10 ]; do
-    
-    		b=$b$NUM
-    		c=$c$NUM
-    		
-		if [ -f $f2$c$ext ]; then
-       			
+	NUM=1
+
+	while [ $NUM -le 9 ]; do
+
+    		bb=$b$NUM
+    		cc=$c$NUM
+		file=$f2$cc$ext
+
+		if [ -f $file ]; then
+
 			# recopy external ID y Encabezados
-			echo -n  $f1$b,$f1$a,$f2$c, >> $2
+			echo -n  $f1$bb,$f1$a,$f2$cc, >> $2
 
 			# transforma a base64
-			cat $(echo ${f2}$c | tr -d '\r' | tr -d '"').jpg | base64 --wrap=0 >> $2
+			cat $(echo ${f2}$cc | tr -d '\r' | tr -d '"').jpg | base64 --wrap=0 >> $2
 
 			#Carrier return at end of line
 			echo  >> $2
