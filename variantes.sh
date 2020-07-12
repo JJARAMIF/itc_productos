c22434d6ef93dd3dd9e57443ceb0a9fd86924129
jjaramif <jamil.jaramillo@hotmail.com>
2020-07-12 05:14:31 +0000

    Actualizacion

--- variantes.sh
+++ variantes.sh
@@ -21,20 +21,22 @@ while IFS=";" read f1 f2 f3 f4 ; do
 	IFS="-"
 
 	for item in $f4 ; do
-	    case $item in
-		"ne") cad=$cad$ava"01,";;
-		"gr") cad=$cad$ava"02,";;
-		"pl") cad=$cad$ava"03,";;
-		"bl") cad=$cad$ava"04,";;
-		"do") cad=$cad$ava"05,";;
-		"az") cad=$cad$ava"06,";;
-		"li") cad=$cad$ava"07,";;
-		"pi") cad=$cad$ava"08,";;
-		"ro") cad=$cad$ava"09,";;
-		"ve") cad=$cad$ava"10,";;
-		"am") cad=$cad$ava"11,";;
-		*) echo No hay el color $item;;
-	    esac
+	    if (( ${#item} > 1 )); then
+	    	case $item in
+		    "ne") cad=$cad$ava"01,";;
+		    "gr") cad=$cad$ava"02,";;
+		    "pl") cad=$cad$ava"03,";;
+		    "bl") cad=$cad$ava"04,";;
+		    "do") cad=$cad$ava"05,";;
+		    "az") cad=$cad$ava"06,";;
+		    "li") cad=$cad$ava"07,";;
+		    "pi") cad=$cad$ava"08,";;
+		    "ro") cad=$cad$ava"09,";;
+		    "ve") cad=$cad$ava"10,";;
+		    "am") cad=$cad$ava"11,";;
+		    *) echo No existe el color $item
+	    	esac
+	    fi
 	done
 
 	IFS=$bakIFS
