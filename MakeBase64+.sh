adb99068a88bf7fd827cceb57b391c130645b3f7
jjaramif <jamil.jaramillo@hotmail.com>
2020-07-12 00:57:25 +0000

    update

--- MakeBase64+.sh
+++ MakeBase64+.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# Usage : MakeBase64+.sh product_imagen.csv product_base64+.csv
-# product_imagen.csv columns are : externalID, jpg, nombre
-# product_imagen.csv separator MUST BE ;
+# Usage : MakeBase64+.sh productos.csv product_base64+.csv
+# productos.csv columns are : externalID, nombre, jpg, color
+# productos.csv separator MUST BE ;
 
 
 ext=".jpg"
@@ -12,7 +12,7 @@ c="-0"
 
 echo \"External ID\",\"product_tmpl_id/id\",\"name\",\"Image\" > $2
 
-while IFS=";" read f1 f2 f3 ; do
+while IFS=";" read f1 f2 f3 f4 ; do
 
 	NUM=1
 
@@ -20,15 +20,15 @@ while IFS=";" read f1 f2 f3 ; do
 
     		bb=$b$NUM
     		cc=$c$NUM
-		file=$f2$cc$ext
+		file=$f3$cc$ext
 
 		if [ -f $file ]; then
 
 			# recopy external ID y Encabezados
-			echo -n  $f1$bb,$f1$a,$f2$cc, >> $2
+			echo -n  $f1$bb,$f1$a,$f3$cc, >> $2
 
 			# transforma a base64
-			cat $(echo ${f2}$cc | tr -d '\r' | tr -d '"').jpg | base64 --wrap=0 >> $2
+			cat $(echo ${f3}$cc | tr -d '\r' | tr -d '"').jpg | base64 --wrap=0 >> $2
 
 			#Carrier return at end of line
 			echo  >> $2
--- MakeBase64.sh
+++ MakeBase64.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 # Usage : MakeBase64.sh product_imagen.csv product_base64.csv
-# product_imagen.csv columns are : externalID, jpg, nombre
+# product_imagen.csv columns are : externalID, nombre, jpg, color
 # product_imagen.csv separator MUST BE ;
 
 ext=".jpg"
 
 echo \"External ID\",\"Image\" > $2
 
-while IFS=";" read f1 f2 f3 ; do
+while IFS=";" read f1 f2 f3 f4 ; do
 
-    if [ -f $f2$ext ]; then
+    if [ -f $f3$ext ]; then
 
 	# recopy external ID
 	echo -n $f1, >> $2
 
 	#If second  column represents the key to match with the filename, please use this command
-	cat $(echo ${f2} | tr -d '\r' | tr -d '"').jpg | base64 --wrap=0 >> $2
+	cat $(echo ${f3} | tr -d '\r' | tr -d '"').jpg | base64 --wrap=0 >> $2
 
 	#Carrier return at end of line
 	echo  >> $2
