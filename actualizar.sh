1c23d26db162600170cc9126e2ebbb73e87f748e
jamil jaramillo <innova_ec@hotmail.com>
2020-07-11 20:12:49 -0500

    Update actualizar.sh

--- actualizar.sh
+++ actualizar.sh
@@ -2,7 +2,7 @@
 git pull
 ./MakeBase64.sh productos.csv product.base64.csv
 ./MakeBase64+.sh productos.csv product.base64+.csv
-./MakeBase64+.sh productos.csv product.template.attribute.line.csv
+./variantes.sh productos.csv product.template.attribute.line.csv
 git add *
 git commit -m "Actualizacion"
 git push
